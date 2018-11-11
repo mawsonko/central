0c9102fdc8ad62d2355ed3789ef730f5aeff0a1d
Sarani SONKO <mawsonko@gmail.com>
2018-11-11 21:19:43 +0100

    First commit

--- src/main/java/com/fi/central/CentralService.java
+++ src/main/java/com/fi/central/CentralService.java
@@ -1,7 +1,14 @@
 package com.fi.central;
 
+import java.util.Iterator;
+import java.util.Properties;
+
 public class CentralService {
     public static void main (String[] args){
-        System.out.println("Hello World");
+        System.out.println("Starting ...");
+        //load hosts
+        Properties properties = new Properties();
+        //browse hosts and send the command for each one
+        System.out.println("Done !!!");
     }
 }
